b81fdf428aff106753dc1222f0010901569d3700
Omar Costilla <omarcostilla6@gmail.com>
2022-05-31 22:36:17 -0700

    dynamic floating action button

---
+++ lib/widget/article_bookmark_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ArticleBookmarkButton extends StatefulWidget {
+  const ArticleBookmarkButton({Key? key}) : super(key: key);
+
+  @override
+  State<ArticleBookmarkButton> createState() => _ArticleBookmarkButtonState();
+}
+
+class _ArticleBookmarkButtonState extends State<ArticleBookmarkButton> {
+  late bool isBookmarked;
+  final booked = Icon(Icons.bookmark);
+  final unBooked = Icon(Icons.bookmark_border);
+  late Widget bookmarkIcon;
+
+  @override
+  void initState() {
+    // TODO:
+    // The values here would be initialized with data from Firebase, letting us
+    // know which ones are already bookmarked and which one's arent
+    isBookmarked = false;
+    bookmarkIcon = unBooked;
+    super.initState();
+  }
+
+  void _toggleBookmark() {
+    setState(() {
+      // TODO: update Firebase
+      isBookmarked = !isBookmarked;
+      if (isBookmarked) {
+        bookmarkIcon = booked;
+      } else {
+        bookmarkIcon = unBooked;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return (IconButton(
+      iconSize: 20,
+      icon: bookmarkIcon,
+      onPressed: () {
+        _toggleBookmark();
+      },
+      color: Theme.of(context).colorScheme.primary,
+    ));
+  }
+}
